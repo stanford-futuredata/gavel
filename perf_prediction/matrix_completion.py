14eb505eb13f29106a0d26a117960ac8493d6fd2
Deepak Narayanan <deepakn94@gmail.com>
2018-12-20 09:48:12 +0530

    Matrix completion baseline to see how easily packed application performance can be predicted (#1)
    - Create a table of performance of (perf_A, perf_B) when run concurrently on the GPU
    - Given baseline_A and baseline_B (performance of applications A and B with no other application running), transform each entry in above table to a single floating point value (perf_A / baseline_A + perf_B / baseline_B)
    - Given this matrix of performance for packed applications, can run an off-the-shelf matrix completion library to recover missing entries

---
+++ perf_prediction/matrix_completion.py
@@ -0,0 +1,51 @@
+import argparse
+import numpy as np
+import pickle
+import random
+
+from fancyimpute import NuclearNormMinimization
+
+def matrix_completion(matrices_filename, p_drops, num_trials):
+    matrices = pickle.load(open(matrices_filename, 'rb'))
+    for gpu_architecture in matrices:
+        print "===================================================================="
+        print "                              %s" % gpu_architecture
+        print "===================================================================="
+        print(np.array(matrices[gpu_architecture]))
+        if gpu_architecture == "K80": continue  # No app combinations in this matrix.
+        missing_entries = []
+        for p_drop in p_drops:
+            mses = []
+            for trial in range(num_trials):
+                original_matrix = np.array(matrices[gpu_architecture])
+
+                matrix_with_missing_entries = original_matrix.copy()
+                (m, n) = matrix_with_missing_entries.shape
+                for i in range(m):
+                    for j in range(n):
+                        if random.uniform(0, 1) < p_drop:
+                            matrix_with_missing_entries[(i, j)] = np.NaN
+                            missing_entries.append((i, j))
+
+                predicted_matrix = NuclearNormMinimization(
+                    verbose=False).fit_transform(matrix_with_missing_entries)
+                error = []
+                for (i, j) in missing_entries:
+                    error.append(original_matrix[(i, j)] - predicted_matrix[(i, j)])
+                mses.append((np.array(error) ** 2).mean())
+            print "Average MSE with p_{drop}=%.1f: %.3f" % (p_drop,
+                                                            np.array(mses).mean())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Perform matrix completion on parsed matrices')
+    parser.add_argument('-f', "--filename", required=True,
+                        help="Input file which contains performance matrices")
+    parser.add_argument('-p', "--p_drops", nargs='+', type=float,
+                        help="List of all p_drops to evaluate")
+    parser.add_argument('-n', "--num_trials", default=10, type=int,
+                        help="Number of trials to perform")
+    args = parser.parse_args()
+
+    matrix_completion(args.filename, args.p_drops,
+                      args.num_trials)
