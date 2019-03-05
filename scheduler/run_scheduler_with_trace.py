924700684b358233742b657e13bedfd8e487147f
Deepak Narayanan <deepakn94@gmail.com>
2019-03-04 22:38:49 -0800

    Add comment, and num_epochs -> num_steps

--- scheduler/run_scheduler_with_trace.py
+++ scheduler/run_scheduler_with_trace.py
@@ -11,29 +11,31 @@ class TestPolicy:
         (m, n) = throughputs.shape
         return np.full((m, n), 1.0 / m)
 
-def get_num_epochs_to_run(job_id, worker_type):
+def get_num_steps_to_run(job_id, worker_type):
     return 1
 
 def read_trace(trace_filename):
-    commands_and_num_epochs = []
+    commands_and_num_steps = []
+    # Trace file is expected to be in the following format:
+    # <timestamp at which job is enqueued> <tab> <command> <tab> <number of times to run command>.
     with open(trace_filename, 'r') as f:
-       for command_and_num_epochs in f.read().strip().split('\n'):
-           [timestamp, command, num_epochs] = command_and_num_epochs.split('\t')
+       for command_and_num_steps in f.read().strip().split('\n'):
+           [timestamp, command, num_steps] = command_and_num_steps.split('\t')
            timestamp = int(timestamp)
-           num_epochs = int(num_epochs)
-           commands_and_num_epochs.append((timestamp, command, num_epochs))
-    commands_and_num_epochs.sort(key=lambda x: x[0])
-    return commands_and_num_epochs
+           num_steps = int(num_steps)
+           commands_and_num_steps.append((timestamp, command, num_steps))
+    commands_and_num_steps.sort(key=lambda x: x[0])
+    return commands_and_num_steps
 
 def main(trace_filename, min_workers, sleep_seconds):
     prev_timestamp = None
-    s = scheduler.Scheduler(TestPolicy(), get_num_epochs_to_run,
+    s = scheduler.Scheduler(TestPolicy(), get_num_steps_to_run,
                             min_workers=min_workers)
     start = time.time()
-    for (timestamp, command, num_epochs) in read_trace(trace_filename):
+    for (timestamp, command, num_steps) in read_trace(trace_filename):
         if prev_timestamp is not None:
             time.sleep(timestamp - prev_timestamp)
-        job_id = s.add_job(command, num_epochs)
+        job_id = s.add_job(command, num_steps)
         prev_timestamp = timestamp
 
     while s.num_jobs() > 0:
