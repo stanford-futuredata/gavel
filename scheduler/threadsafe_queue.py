225997d59793d299d2cecb12b4ed851bea28a708
Deepak Narayanan <deepakn94@gmail.com>
2019-03-05 09:34:12 -0800

    tensor -> item in threadsafe_queue.py

--- scheduler/threadsafe_queue.py
+++ scheduler/threadsafe_queue.py
@@ -1,16 +1,16 @@
 import threading
 
 """
-Implementation of a thread-safe queue with one producer and one consumer.
+Implementation of a thread-safe queue.
 """
 class Queue:
     def __init__(self):
         self.queue = []
         self.cv = threading.Condition()
 
-    def add(self, tensor):
+    def add(self, item):
         self.cv.acquire()
-        self.queue.append(tensor)
+        self.queue.append(item)
         self.cv.notify()
         self.cv.release()
 
@@ -18,6 +18,6 @@ class Queue:
         self.cv.acquire()
         while len(self.queue) == 0:
             self.cv.wait()
-        tensor = self.queue.pop(0)
+        item = self.queue.pop(0)
         self.cv.release()
-        return tensor
+        return item
