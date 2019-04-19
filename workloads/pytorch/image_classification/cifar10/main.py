87a67c948541740f026f82b1a0151e96ad61a82c
Keshav Santhanam <keshav2@stanford.edu>
2019-04-19 01:00:01 +0000

    epochs -> steps

--- workloads/pytorch/image_classification/cifar10/main.py
+++ workloads/pytorch/image_classification/cifar10/main.py
@@ -1,42 +1,40 @@
 '''Train CIFAR10 with PyTorch.'''
 from __future__ import print_function
 
-print('flag1')
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 
-print('flag2')
-
 import torchvision
 import torchvision.transforms as transforms
-
-print('flag3')
 
 import os
 import argparse
 
-print('flag4')
-
 from models import *
 from utils import progress_bar
 
-print('flag5')
-
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--data_dir', required=True, type=str, help='Data directory')
-parser.add_argument('--num_epochs', required=True, type=int, help='Number of epochs to train for')
+parser.add_argument('--num_epochs', default=None, type=int, help='Number of epochs to train for')
+parser.add_argument('--num_steps', default=None, type=int, help='Number of steps to train for')
 parser.add_argument('--batch_size', default=64, type=int, help='Batch size')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 parser.add_argument('--use_progress_bar', '-p', action='store_true', default=False, help='Use progress bar')
+parser.add_argument('--log_interval', type=int, default=100,
+                    help='Interval to log')
 args = parser.parse_args()
 
-print('flag6')
+
 print('==> Starting script..')
+
+if args.num_epochs is not None and args.num_steps is not None:
+    raise ValueError('Only one of num_epochs and num_steps may be set')
+elif args.num_epochs is None and args.num_steps is None:
+    raise ValueError('One of num_epochs and num_steps must be set')
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
@@ -96,12 +94,13 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 
 # Training
-def train(epoch):
+def train(epoch, cumulative_steps=None):
     print('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
     correct = 0
     total = 0
+    done = False
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
@@ -118,6 +117,18 @@ def train(epoch):
         if args.use_progress_bar:
           progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
               % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        elif batch_idx % args.log_interval == 0 and batch_idx > 0:
+            print('Batch: %d, Loss: %.3f, '
+                  'Acc: %.3f%% (%d/%d)' % (batch_idx, train_loss/(batch_idx+1),
+                                           100.*correct/total, correct, total))
+        if cumulative_steps is not None:
+            cumulative_steps += 1
+            if args.num_steps is not None and cumulative_steps >= args.num_steps:
+                done = True
+                break
+    return (cumulative_steps, done)
+
+
 
 def test(epoch):
     global best_acc
@@ -155,6 +166,11 @@ def test(epoch):
         best_acc = acc
     """
 
+if args.num_epochs is None:
+    args.num_epochs = int(math.ceil(float(args.num_steps) / len(trainloader)))
+cumulative_steps = 0
 for epoch in range(start_epoch, args.num_epochs):
-    train(epoch)
+    cumulative_steps, done = train(epoch, cumulative_steps)
+    if done:
+        break
     #test(epoch)
