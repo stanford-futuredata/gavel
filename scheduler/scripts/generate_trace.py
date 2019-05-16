81a407bb35a4c3f7e897a067e8792753cb045404
Deepak Narayanan <deepakn94@gmail.com>
2019-05-16 16:29:39 -0700

    Reorganize scripts

--- scheduler/scripts/generate_trace.py
+++ scheduler/scripts/generate_trace.py
@@ -1,52 +1,88 @@
 import argparse
-import numpy
+import numpy as np
 import random
 
+INITIAL_DELAY = 10
+MAX_JOB_STEPS = 10000000
 
-models = [
-    "vgg16",
-    "nmt",
-    "inception3",
-    "vae",
-    "resnet50",
-    "lm_large",
+random.seed(42)
+np.random.seed(42)
+
+jobs = [
+    ('ResNet-50', ('cd /home/keshavsanthanam/gpusched/workloads/pytorch/'
+      'image_classification/imagenet && python3 '
+      'main.py -j 4 -a resnet50 -b 64 /home/deepakn94/imagenet/'),
+     '--num_minibatches'),
+    ('A3C', ('cd /home/keshavsanthanam/gpusched/workloads/pytorch/rl && '
+       'python3 main.py --env PongDeterministic-v4 --workers 4 --amsgrad True'),
+      '--max-steps'),
+    ('LM', ('cd /home/keshavsanthanam/gpusched/workloads/pytorch/language_modeling '
+      '&& python main.py --cuda --data /home/keshavsanthanam/data/wikitext-2'),
+      '--steps'),
+    ('Recommendation', ('cd /home/keshavsanthanam/gpusched/workloads/pytorch/'
+      'recommendation/scripts/ml-20m && python3 train.py'), '-n'),
+    ('ResNet-18', ('cd /home/keshavsanthanam/gpusched/workloads/pytorch/'
+      'image_classification/cifar10 && python3 '
+      'main.py --data_dir=/home/keshavsanthanam/data/cifar10'),
+      '--num_steps'),
+    ('Transformer', ('cd /home/keshavsanthanam/gpusched/workloads/pytorch/translation/ '
+      '&& python3 train.py -data /home/keshavsanthanam/data/translation/'
+      'multi30k.atok.low.pt -proj_share_weight'), '-step'),
+    ('CycleGAN', ('cd /home/keshavsanthanam/gpusched/workloads/pytorch/cyclegan '
+      '&& python3 cyclegan.py --dataset_path /home/keshavsanthanam/data'
+      '/monet2photo --decay_epoch 0'), '--n_steps'),
 ]
 
+throughputs = {
+        'ResNet-50': 1.333386669,
+        'ResNet-18': 46.45544922,
+        'CycleGAN': 3.497604141,
+        'A3C': 6.035731531,
+        'Transformer': 8.26514588,
+        'Recommendation': 0.4535558781,
+        'LM': 0.7638835841,
+}
 
-def generate_trace(arrival_process, lam, trace_size):
-    if arrival_process == "poisson":
-        assert(lam is not None)
-        inter_arrival_times = list(numpy.random.poisson(lam, trace_size-1))
-        arrival_times = [0]
-        for inter_arrival_time in inter_arrival_times:
-            arrival_times.append(arrival_times[-1] + inter_arrival_time)
-    elif arrival_process == "constant":
-        arrival_times = [0] * trace_size
-    else:
-        raise Exception("Invalid arrival process")
+"""
+    (('cd /home/keshavsanthanam/gpusched/workloads/tensorflow/translation/ '
+      '&& python -m nmt.nmt --src=vi --tgt=en '
+      '--vocab_prefix=/home/keshavsanthanam/data/nmt_data/vocab '
+      '--train_prefix=/home/keshavsanthanam/data/nmt_data/train '
+      '--dev_prefix=/home/keshavsanthanam/data/nmt_data/tst2012 '
+      '--test_prefix=/home/keshavsanthanam/data/nmt_data/tst2013 '
+      '--num_layers=2 --num_units=128 --dropout=0.2 '
+      '--metrics=bleu'), '--num_train_steps'),
+    (('cd /home/keshavsanthanam/gpusched/workloads/tensorflow/'
+      'language_modeling && python ptb_word_lm.py '
+      '--data_path=/home/keshavsanthanam/data/ptb --model=medium',
+      '--max_steps')),
+"""
 
-    for arrival_time in arrival_times:
-        random_idx = random.randint(0, len(models)-1)
-        model_name = models[random_idx]
-        duration = 10 ** random.uniform(0, 4)  # this is in minutes.
-        duration *= 60
-        # Split into 200 steps.
-        duration /= 200.
-        num_epochs = 200
-        cmd = f"echo \"{model_name}\""
-        print(f"{arrival_time}\t{model_name}\t{cmd}\t{duration}\t{num_epochs}")
+def generate(lam, N, initial_delay, output_file):
+    samples = np.random.poisson(lam, size=N)
+    arrival_times = [np.sum(samples[0:i]) for i in range(N)]
+    with open(output_file, 'w') as f:
+        for arrival_time in arrival_times:
+            job = random.choice(jobs)
+            duration = 10 ** random.uniform(0, 2)  # this is in minutes.
+            duration *= 60
+            total_steps = int(duration * throughputs[job[0]])
+            # 'job_type\tcommand\tnum_steps_arg\ttotal_steps\tarrival_time'
+            f.write('%s\t%s\t%s\t%d\t%d\n' % (job[0], job[1], job[2],
+                                              total_steps,
+                                              arrival_time + initial_delay))
 
+def main(args):
+    generate(args.lam, args.num_jobs, args.initial_delay, args.output_file)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description="Generate a trace"
-    )
-    parser.add_argument('-a', "--arrival_process", type=str, required=True,
-                        help="Arrival process: poisson|constant")
-    parser.add_argument('-l', "--lam", type=float, default=None,
-                        help="Lambda parameter for Poisson arrival process")
-    parser.add_argument('-s', "--trace_size", type=int, required=True,
-                        help="Trace size")
-    args = parser.parse_args()
-
-    generate_trace(args.arrival_process, args.lam, args.trace_size)
+if __name__=='__main__':
+   parser = argparse.ArgumentParser(description='Generate scheduler trace')
+   parser.add_argument('-l', '--lam', type=float, default=1,
+                       help='Lambda value for Poisson arrival process')
+   parser.add_argument('-n', '--num_jobs', type=int, default=50,
+                       help='Number of jobs')
+   parser.add_argument('-o', '--output_file', type=str, required=True,
+                       help='File to output trace to')
+   parser.add_argument('-i', '--initial_delay', type=int, default=10,
+                       help='Initial job arrival time delay')
+   main(parser.parse_args())
