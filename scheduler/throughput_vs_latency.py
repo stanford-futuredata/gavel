e371cbb1742e0caca22b8078a985c776d3cd7303
Keshav Santhanam <keshav2@stanford.edu>
2019-07-17 15:23:50 -0700

    Cleaned up throughput_vs_latency

--- scheduler/throughput_vs_latency.py
+++ scheduler/throughput_vs_latency.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 
 import job
+from job_id_pair import JobIdPair
 import policies
 import scheduler
 
@@ -37,8 +38,8 @@ def parse_trace(trace_file, throughputs_file):
 
     with open(trace_file, 'r') as f:
         for line in f:
-            job_type, command, num_steps_arg, total_steps, arrival_time, scale_factor = \
-                    line.split('\t')
+            (job_type, command, num_steps_arg, total_steps,
+             arrival_time, scale_factor) = line.split('\t')
             jobs.append(job.Job(job_id=None,
                                 job_type=job_type,
                                 command=command,
@@ -47,7 +48,8 @@ def parse_trace(trace_file, throughputs_file):
                                 duration=None,
                                 scale_factor=int(scale_factor)))
             arrival_times.append(int(arrival_time))
-            durations.append(int(total_steps) / float(throughputs['k80'][job_type]['null']))
+            v100_throughput = float(throughputs['k80'][job_type]['null'])
+            durations.append(int(total_steps) / v100_throughput)
     return jobs, arrival_times, durations
 
 
@@ -119,10 +121,10 @@ def debug_packing_for_rounds_with_timelines():
 
 def main():
     # TODO: convert to command line arguments
-    schedule_in_rounds = True
+    schedule_in_rounds = False
     throughputs_file = 'combined_throughputs.json'
     num_v100s = 25
-    policy_names = ['max_min_fairness']
+    policy_names = ['fifo']
     ratios = [
             {'v100': 1, 'p100': 0, 'k80': 0},
             #{'v100': 1, 'p100': 1, 'k80': 0},
@@ -132,46 +134,53 @@ def main():
     job_range = (0, 100)
     with open(throughputs_file, 'r') as f:
         throughputs = json.load(f)
-    lams = [1024]#[256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072]
+    lams = [16384]
     for ratio in ratios:
         cluster_spec = {}
         total_gpu_fraction = sum([ratio[gpu_type] for gpu_type in ratio])
         for gpu_type in ratio:
-            cluster_spec[gpu_type] = int(ratio[gpu_type] / total_gpu_fraction * num_v100s)
+            fraction = ratio[gpu_type] / total_gpu_fraction
+            cluster_spec[gpu_type] = int(fraction * num_v100s)
         all_lams_str = ','.join([str(lam) for lam in lams])
-        output_file = ('latency_vs_throughput_cluster_ratio_'
-                       '%d_%d_%d_lambda=%s_jobs_%d-%d.csv') % (ratio['v100'],
-                                                               ratio['p100'],
-                                                               ratio['k80'],
-                                                               all_lams_str,
-                                                               job_range[0],
-                                                               job_range[1])
+        output_file = ('latency_vs_throughput_cluster_spec_'
+                       '%d_%d_%d_lambda=%s_'
+                       'jobs_to_complete_%d-%d.csv') % (cluster_spec['v100'],
+                                                        cluster_spec['p100'],
+                                                        cluster_spec['k80'],
+                                                        all_lams_str,
+                                                        job_range[0],
+                                                        job_range[1])
         with open(output_file, 'w') as f:
-            f.write('# v100,# p100,# k80,Rounds,Policy,Lambda,Utilization,Average JCT\n')
+            f.write('# v100,# p100,# k80,Rounds,Policy,Lambda,Utilization,'
+                    'Average JCT\n')
             for use_rounds in [True]:
                 for policy_name in policy_names:
                     for lam in lams:
                         # TODO: can this be moved to previous for loop?
                         policy = get_policy(policy_name)
-                        sched = \
-                            scheduler.Scheduler(policy,
-                                                schedule_in_rounds=use_rounds,
-                                                throughputs_file=throughputs_file,
-                                                emulate=True)
-                        jobs_to_complete = set(list(range(job_range[0], job_range[1])))
-                        sched.emulate_with_generated_jobs(cluster_spec,
-                                                          throughputs, lam,
-                                                          jobs_to_complete)
+                        sched = scheduler.Scheduler(
+                                policy, schedule_in_rounds=use_rounds,
+                                throughputs_file=throughputs_file,
+                                emulate=True)
+                        jobs_to_complete = set()
+                        for i in range(job_range[0], job_range[1]):
+                            jobs_to_complete.add(JobIdPair(i, None))
+                        sched.emulate(cluster_spec,
+                                      throughputs=throughputs,
+                                      lam=lam,
+                                      jobs_to_complete=jobs_to_complete)
                         utilization = sched.get_cluster_utilization()
                         average_jct = sched.get_average_jct(jobs_to_complete)
-                        f.write('%d,%d,%d,%s,%s,%d,%.3f,%.3f\n' % (cluster_spec['v100'],
-                                                                   cluster_spec['p100'],
-                                                                   cluster_spec['k80'],
-                                                                   str(use_rounds),
-                                                                   policy.name,
-                                                                   lam,
-                                                                   utilization,
-                                                                   average_jct))
+                        average_jct = sched.get_average_jct()
+                        f.write('%d,%d,%d,%s,%s,%d,'
+                                '%.3f,%.3f\n' % (cluster_spec['v100'],
+                                                 cluster_spec['p100'],
+                                                 cluster_spec['k80'],
+                                                 str(use_rounds),
+                                                 policy.name,
+                                                 lam,
+                                                 utilization,
+                                                 average_jct))
                         f.flush()
 
 
