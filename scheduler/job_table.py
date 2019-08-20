ffdee3d98177160857f830adbc1bfd92ecf339c4
Keshav Santhanam <keshav2@stanford.edu>
2019-08-20 10:56:19 -0700

    Add script for doing throughput estimation sweep with pre-predicted throughputs

--- scheduler/job_table.py
+++ scheduler/job_table.py
@@ -1,37 +1,179 @@
 from job_template import JobTemplate
 
-JobTable = [
-    JobTemplate(model='ResNet-18',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'image_classification/cifar10 && python3 '
-                       'main.py --data_dir=%s/data/cifar10'),
-              num_steps_arg='--num_steps'),
-    JobTemplate(model='ResNet-50',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'image_classification/imagenet && python3 '
-                       'main.py -j 4 -a resnet50 -b 64 '
-                       '%s/data/imagenet/pytorch'),
-              num_steps_arg='--num_minibatches'),
-    JobTemplate(model='Transformer',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'translation && python3 train.py -data '
-                       '%s/data/translation/multi30k.atok.low.pt'
-                       '-proj_share_weight'),
-              num_steps_arg='-step'),
-    JobTemplate(model='A3C',
-              command=('cd %s/gpusched/workloads/pytorch/rl && '
-                       'python3 main.py --env PongDeterministic-v4 --workers 4 '
-                       '--amsgrad True'),
-              num_steps_arg='--max-steps',
-              needs_data_dir=False),
-    JobTemplate(model='Recommendation',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'recommendation/scripts/ml-20m && python3 train.py '
-                       '--data_dir %s/data/ml-20m/pro_sg/'),
-              num_steps_arg='-n'),
-    JobTemplate(model='CycleGAN',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'cyclegan && python3 cyclegan.py --dataset_path '
-                       '%s/data/monet2photo --decay_epoch 0'),
-              num_steps_arg='--n_steps'),
-]
+# TODO: Figure out a better way of toggling between the two tables.
+THROUGHPUT_ESTIMATION = False
+
+if THROUGHPUT_ESTIMATION:
+    JobTable = [
+        JobTemplate(model='ResNet-18 (batch size 16)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'image_classification/cifar10 && python3 '
+                           'main.py --data_dir=%s/data/cifar10'),
+                  num_steps_arg='--num_steps'),
+        JobTemplate(model='ResNet-18 (batch size 32)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'image_classification/cifar10 && python3 '
+                           'main.py --data_dir=%s/data/cifar10'),
+                  num_steps_arg='--num_steps'),
+        JobTemplate(model='ResNet-18 (batch size 64)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'image_classification/cifar10 && python3 '
+                           'main.py --data_dir=%s/data/cifar10'),
+                  num_steps_arg='--num_steps'),
+        JobTemplate(model='ResNet-18 (batch size 128)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'image_classification/cifar10 && python3 '
+                           'main.py --data_dir=%s/data/cifar10'),
+                  num_steps_arg='--num_steps'),
+        JobTemplate(model='ResNet-18 (batch size 256)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'image_classification/cifar10 && python3 '
+                           'main.py --data_dir=%s/data/cifar10'),
+                  num_steps_arg='--num_steps'),
+        JobTemplate(model='ResNet-50 (batch size 16)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'image_classification/imagenet && python3 '
+                           'main.py -j 4 -a resnet50 -b 64 '
+                           '%s/data/imagenet/pytorch'),
+                  num_steps_arg='--num_minibatches'),
+        JobTemplate(model='ResNet-50 (batch size 32)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'image_classification/imagenet && python3 '
+                           'main.py -j 4 -a resnet50 -b 64 '
+                           '%s/data/imagenet/pytorch'),
+                  num_steps_arg='--num_minibatches'),
+        JobTemplate(model='ResNet-50 (batch size 64)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'image_classification/imagenet && python3 '
+                           'main.py -j 4 -a resnet50 -b 64 '
+                           '%s/data/imagenet/pytorch'),
+                  num_steps_arg='--num_minibatches'),
+        JobTemplate(model='ResNet-50 (batch size 128)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'image_classification/imagenet && python3 '
+                           'main.py -j 4 -a resnet50 -b 64 '
+                           '%s/data/imagenet/pytorch'),
+                  num_steps_arg='--num_minibatches'),
+        JobTemplate(model='Transformer (batch size 16)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'translation && python3 train.py -data '
+                           '%s/data/translation/multi30k.atok.low.pt'
+                           '-proj_share_weight'),
+                  num_steps_arg='-step'),
+        JobTemplate(model='Transformer (batch size 32)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'translation && python3 train.py -data '
+                           '%s/data/translation/multi30k.atok.low.pt'
+                           '-proj_share_weight'),
+                  num_steps_arg='-step'),
+        JobTemplate(model='Transformer (batch size 64)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'translation && python3 train.py -data '
+                           '%s/data/translation/multi30k.atok.low.pt'
+                           '-proj_share_weight'),
+                  num_steps_arg='-step'),
+        JobTemplate(model='Transformer (batch size 128)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'translation && python3 train.py -data '
+                           '%s/data/translation/multi30k.atok.low.pt'
+                           '-proj_share_weight'),
+                  num_steps_arg='-step'),
+        JobTemplate(model='Transformer (batch size 256)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'translation && python3 train.py -data '
+                           '%s/data/translation/multi30k.atok.low.pt'
+                           '-proj_share_weight'),
+                  num_steps_arg='-step'),
+        JobTemplate(model='A3C',
+                  command=('cd %s/gpusched/workloads/pytorch/rl && '
+                           'python3 main.py --env PongDeterministic-v4 --workers 4 '
+                           '--amsgrad True'),
+                  num_steps_arg='--max-steps',
+                  needs_data_dir=False),
+        JobTemplate(model='LM (batch size 5)',
+                    command=('cd %s && ./placeholder_command'),
+                    num_steps_arg='--placeholder_steps',
+                    needs_data_dir=False),
+        JobTemplate(model='LM (batch size 10)',
+                    command=('cd %s && ./placeholder_command'),
+                    num_steps_arg='--placeholder_steps',
+                    needs_data_dir=False),
+        JobTemplate(model='LM (batch size 20)',
+                    command=('cd %s && ./placeholder_command'),
+                    num_steps_arg='--placeholder_steps',
+                    needs_data_dir=False),
+        JobTemplate(model='LM (batch size 40)',
+                    command=('cd %s && ./placeholder_command'),
+                    num_steps_arg='--placeholder_steps',
+                    needs_data_dir=False),
+        JobTemplate(model='LM (batch size 80)',
+                    command=('cd %s && ./placeholder_command'),
+                    num_steps_arg='--placeholder_steps',
+                    needs_data_dir=False),
+        JobTemplate(model='Recommendation (batch size 512)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'recommendation/scripts/ml-20m && python3 train.py '
+                           '--data_dir %s/data/ml-20m/pro_sg/'),
+                  num_steps_arg='-n'),
+        JobTemplate(model='Recommendation (batch size 1024)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'recommendation/scripts/ml-20m && python3 train.py '
+                           '--data_dir %s/data/ml-20m/pro_sg/'),
+                  num_steps_arg='-n'),
+        JobTemplate(model='Recommendation (batch size 2048)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'recommendation/scripts/ml-20m && python3 train.py '
+                           '--data_dir %s/data/ml-20m/pro_sg/'),
+                  num_steps_arg='-n'),
+        JobTemplate(model='Recommendation (batch size 4096)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'recommendation/scripts/ml-20m && python3 train.py '
+                           '--data_dir %s/data/ml-20m/pro_sg/'),
+                  num_steps_arg='-n'),
+        JobTemplate(model='Recommendation (batch size 8192)',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'recommendation/scripts/ml-20m && python3 train.py '
+                           '--data_dir %s/data/ml-20m/pro_sg/'),
+                  num_steps_arg='-n'),
+        JobTemplate(model='CycleGAN',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'cyclegan && python3 cyclegan.py --dataset_path '
+                           '%s/data/monet2photo --decay_epoch 0'),
+                  num_steps_arg='--n_steps'),
+    ]
+else:
+    JobTable = [
+        JobTemplate(model='ResNet-18',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'image_classification/cifar10 && python3 '
+                           'main.py --data_dir=%s/data/cifar10'),
+                  num_steps_arg='--num_steps'),
+        JobTemplate(model='ResNet-50',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'image_classification/imagenet && python3 '
+                           'main.py -j 4 -a resnet50 -b 64 '
+                           '%s/data/imagenet/pytorch'),
+                  num_steps_arg='--num_minibatches'),
+        JobTemplate(model='Transformer',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'translation && python3 train.py -data '
+                           '%s/data/translation/multi30k.atok.low.pt'
+                           '-proj_share_weight'),
+                  num_steps_arg='-step'),
+        JobTemplate(model='A3C',
+                  command=('cd %s/gpusched/workloads/pytorch/rl && '
+                           'python3 main.py --env PongDeterministic-v4 --workers 4 '
+                           '--amsgrad True'),
+                  num_steps_arg='--max-steps',
+                  needs_data_dir=False),
+        JobTemplate(model='Recommendation',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'recommendation/scripts/ml-20m && python3 train.py '
+                           '--data_dir %s/data/ml-20m/pro_sg/'),
+                  num_steps_arg='-n'),
+        JobTemplate(model='CycleGAN',
+                  command=('cd %s/gpusched/workloads/pytorch/'
+                           'cyclegan && python3 cyclegan.py --dataset_path '
+                           '%s/data/monet2photo --decay_epoch 0'),
+                  num_steps_arg='--n_steps'),
+    ]
---
+++ scheduler/run_throughput_estimation_sweep_with_pre_predicted_throughputs.py
@@ -0,0 +1,294 @@
+import argparse
+import datetime
+import json
+import io
+import contextlib
+from func_timeout import func_timeout, FunctionTimedOut
+import matplotlib.pyplot as plt
+import multiprocessing
+import numpy as np
+import os
+import sys
+
+import job
+from job_id_pair import JobIdPair
+import policies
+import scheduler
+import utils
+
+cutoff_throughputs = {
+    '1:0:0': {
+        'fifo_packing': 0.8,
+        'min_total_duration_packing': 0.8,
+    }
+}
+
+def emulate_with_timeout(experiment_id, policy_name, schedule_in_rounds,
+                         throughputs_file, cluster_spec, lam, seed, interval,
+                         jobs_to_complete, fixed_job_duration, log_dir, timeout,
+                         completion_algo, drop_prob, verbose):
+    f = io.StringIO()
+    lam_str = 'lam=%2f.log' % (lam)
+    with open(os.path.join(log_dir, lam_str), 'w') as f:
+        with contextlib.redirect_stdout(f):
+            policy = utils.get_policy(policy_name, seed)
+            sched = scheduler.Scheduler(
+                            policy,
+                            schedule_in_rounds=schedule_in_rounds,
+                            throughputs_file=throughputs_file,
+                            seed=seed,
+                            time_per_iteration=interval,
+                            emulate=True,
+                            predict_throughputs=False)
+
+            cluster_spec_str = 'v100:%d|p100:%d|k80:%d' % (cluster_spec['v100'],
+                                                           cluster_spec['p100'],
+                                                           cluster_spec['k80'])
+            if verbose:
+                current_time = datetime.datetime.now()
+                print('[%s] [Experiment ID: %2d] '
+                      'Configuration: cluster_spec=%s, policy=%s, '
+                       'seed=%d, lam=%f, completion_algo=%s, '
+                       'drop_prob=%.2f' % (current_time,
+                                           experiment_id,
+                                           cluster_spec_str,
+                                           policy.name,
+                                           seed,
+                                           lam,
+                                           completion_algo,
+                                           drop_prob),
+                      file=sys.stderr)
+
+            if timeout is None:
+                sched.emulate(cluster_spec, lam=lam,
+                              jobs_to_complete=jobs_to_complete,
+                              fixed_job_duration=fixed_job_duration)
+                average_jct = sched.get_average_jct(jobs_to_complete)
+                utilization = sched.get_cluster_utilization()
+            else:
+                try:
+                    func_timeout(timeout, sched.emulate,
+                                 args=(cluster_spec,),
+                                 kwargs={
+                                    'lam': lam,
+                                    'jobs_to_complete': jobs_to_complete,
+                                    'fixed_job_duration': fixed_job_duration,
+                                 })
+                    average_jct = sched.get_average_jct(jobs_to_complete)
+                    utilization = sched.get_cluster_utilization()
+                except FunctionTimedOut:
+                    average_jct = float('inf')
+                    utilization = 1.0
+
+    if verbose:
+        current_time = datetime.datetime.now()
+        print('[%s] [Experiment ID: %2d] '
+              'Results: average JCT=%f, utilization=%f' % (current_time,
+                                                           experiment_id,
+                                                           average_jct,
+                                                           utilization),
+              file=sys.stderr)
+
+    return average_jct, utilization
+
+def emulate_with_timeout_helper(args):
+    emulate_with_timeout(*args)
+
+def get_all_throughput_files(predicted_throughputs_dir):
+    throughput_files = {}
+    algo_dirs = os.listdir(predicted_throughputs_dir)
+    for algo_dir in algo_dirs:
+        _, algo = algo_dir.split('=')
+        throughput_files[algo] = {}
+        algo_dir = os.path.join(predicted_throughputs_dir, algo_dir)
+        seed_dirs = os.listdir(algo_dir)
+        for seed_dir in seed_dirs:
+            _, seed = seed_dir.split('=')
+            seed = int(seed)
+            throughput_files[algo][seed] = {}
+            seed_dir = os.path.join(algo_dir, seed_dir)
+            json_files = os.listdir(seed_dir)
+            for json_file in json_files:
+                drop_prob =\
+                    round(float(json_file.split('=')[1].strip('.json')), 2)
+                json_file = os.path.join(seed_dir, json_file)
+                throughput_files[algo][seed][drop_prob] = json_file
+    return throughput_files
+
+def main(args):
+    if args.window_start >= args.window_end:
+        raise ValueError('Window start must be < than window end.')
+
+    all_throughput_files = get_all_throughput_files('predicted_throughputs')
+    schedule_in_rounds = True
+    num_v100s = args.gpus
+    policy_names = args.policies
+    job_range = (args.window_start, args.window_end)
+    experiment_id = 0
+    #measurement_percentages =\
+    #    np.linspace(0.0, 1.0, num=args.num_measurement_percentages,
+    #                endpoint=True)[1:]
+    throughputs = list(np.linspace(args.throughput_lower_bound,
+                                   args.throughput_upper_bound,
+                                   num=args.num_data_points))
+    if throughputs[0] == 0.0:
+        throughputs = throughputs[1:]
+
+    raw_logs_dir = os.path.join(args.log_dir, 'raw_logs')
+    if not os.path.isdir(raw_logs_dir):
+        os.mkdir(raw_logs_dir)
+
+    jobs_to_complete = set()
+    for i in range(job_range[0], job_range[1]):
+        jobs_to_complete.add(JobIdPair(i, None))
+
+    all_args_list = []
+    for ratio_str in args.ratios:
+        ratio = {}
+        x = ratio_str.split(':')
+        if len(x) != 3:
+            raise ValueError('Invalid cluster ratio %s' % (ratio_str))
+        ratio = {
+            'v100': int(x[0]),
+            'p100': int(x[1]),
+            'k80': int(x[2])
+            }
+        cluster_spec = {}
+        total_gpu_fraction = sum([ratio[gpu_type] for gpu_type in ratio])
+        for gpu_type in ratio:
+            fraction = ratio[gpu_type] / total_gpu_fraction
+            cluster_spec[gpu_type] = int(fraction * num_v100s)
+
+        cluster_spec_str = 'v100=%d.p100=%d.k80=%d' % (cluster_spec['v100'],
+                                                       cluster_spec['p100'],
+                                                       cluster_spec['k80'])
+        raw_logs_cluster_spec_subdir = os.path.join(raw_logs_dir,
+                                                    cluster_spec_str)
+        if not os.path.isdir(raw_logs_cluster_spec_subdir):
+            os.mkdir(raw_logs_cluster_spec_subdir)
+
+        for policy_name in policy_names:
+            raw_logs_policy_subdir = os.path.join(raw_logs_cluster_spec_subdir,
+                                                  policy_name)
+            if not os.path.isdir(raw_logs_policy_subdir):
+                os.mkdir(raw_logs_policy_subdir)
+
+            for completion_algo in args.completion_algorithms:
+                completion_algo_str =\
+                    'completion_algo=%s' % (completion_algo)
+                raw_logs_completion_algo_subdir =\
+                    os.path.join(raw_logs_policy_subdir,
+                                 completion_algo_str)
+                if not os.path.isdir(raw_logs_completion_algo_subdir):
+                    os.mkdir(raw_logs_completion_algo_subdir)
+
+                for drop_prob in args.drop_probs:
+                    drop_prob_str =\
+                        'drop_prob_str=%f' % (drop_prob)
+                    raw_logs_drop_prob_subdir =\
+                        os.path.join(raw_logs_completion_algo_subdir,
+                                     drop_prob_str)
+                    if not os.path.isdir(raw_logs_drop_prob_subdir):
+                        os.mkdir(raw_logs_drop_prob_subdir)
+                    for throughput in throughputs:
+                        if (ratio_str in cutoff_throughputs and
+                            policy_name in cutoff_throughputs[ratio_str]):
+                            cutoff_throughput = \
+                                cutoff_throughputs[ratio_str][policy_name]
+                            if throughput >= cutoff_throughput:
+                                print('Throughput of %f is too high for '
+                                      'policy %s with cluster '
+                                      'ratio %s.' % (throughput, policy_name,
+                                                     ratio_str))
+                                continue
+                        lam = 3600.0 / throughput
+                        for seed in args.seeds:
+                            seed_str = 'seed=%d' % (seed)
+                            raw_logs_seed_subdir = \
+                                os.path.join(raw_logs_drop_prob_subdir,
+                                             seed_str)
+                            if not os.path.isdir(raw_logs_seed_subdir):
+                                os.mkdir(raw_logs_seed_subdir)
+
+                            throughputs_file =\
+                                all_throughput_files[completion_algo][seed][drop_prob]
+                            all_args_list.append((experiment_id, policy_name,
+                                                  schedule_in_rounds,
+                                                  throughputs_file,
+                                                  cluster_spec,
+                                                  lam, seed, args.interval,
+                                                  jobs_to_complete,
+                                                  args.fixed_job_duration,
+                                                  raw_logs_seed_subdir,
+                                                  args.timeout,
+                                                  completion_algo,
+                                                  drop_prob,
+                                                  args.verbose))
+                            experiment_id += 1
+
+    if len(all_args_list) > 0:
+        current_time = datetime.datetime.now()
+        print('[%s] Running %d total experiment(s)...' % (current_time,
+                                                          len(all_args_list)))
+        all_args_list = sorted(all_args_list, key=lambda x: (x[5], x[13]),
+                               reverse=True)
+        with multiprocessing.Pool(args.processes) as p:
+            p.map(emulate_with_timeout_helper, all_args_list)
+    else:
+        raise ValueError('No work to be done!')
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(
+            description='Sweep through throughput estimation parameters.')
+
+    parser.add_argument('-g', '--gpus', type=int, default=25,
+                        help='Number of v100 GPUs')
+    parser.add_argument('-l', '--log-dir', type=str, default='logs',
+                        help='Log directory')
+    parser.add_argument('-s', '--window-start', type=int, default=0,
+                        help='Measurement window start (job ID)')
+    parser.add_argument('-e', '--window-end', type=int, default=5000,
+                        help='Measurement window end (job ID)')
+    parser.add_argument('-t', '--timeout', type=int, default=None,
+                        help='Timeout (in seconds) for each run')
+    parser.add_argument('-j', '--processes', type=int, default=None,
+                        help=('Number of processes to use in pool '
+                              '(use as many as available if not specified)'))
+    parser.add_argument('-p', '--policies', type=str, nargs='+',
+                        choices=['fifo_packed', 'max_min_fairness_packed',
+                                 'min_total_duration_packed'],
+                        default=['fifo_packed', 'max_min_fairness_packed',
+                                 'min_total_duration_packed'],
+                        help='List of policies to sweep')
+    parser.add_argument('-r', '--ratios', type=str, nargs='+',
+                        default=['1:0:0', '1:1:0', '1:1:1', '2:1:0'],
+                        help=('List of cluster ratios to sweep in the form '
+                              '#v100s:#p100s:#k80s'))
+    parser.add_argument('-a', '--throughput-lower-bound', type=float,
+                        default=None,
+                        help='Lower bound for throughput interval to sweep')
+    parser.add_argument('-b', '--throughput-upper-bound', type=float,
+                        default=None,
+                        help='Upper bound for throughput interval to sweep')
+    parser.add_argument('-n', '--num-data-points', type=int, default=20,
+                             help='Number of data points to sweep through')
+    parser.add_argument('--seeds', type=int, nargs='+',
+                        default=[0, 1, 42, 1234, 10],
+                        help='List of random seeds')
+    parser.add_argument('-i', '--interval', type=int, default=1920,
+                        help='Interval length (in seconds)')
+    parser.add_argument('-f', '--fixed-job-duration', type=int, default=None,
+                        help=('If set, fixes the duration of all jobs to the '
+                              'specified value (in seconds)'))
+    parser.add_argument('--completion_algorithms', type=str, nargs='+',
+                        choices=['NN', 'SVT', 'PMF', 'BMF'],
+                        default=['SVT', 'NN', 'PMF', 'BMF'],
+                        help=('Matrix completion algorithm for throughput '
+                              'prediction'))
+    parser.add_argument('--drop_probs', type=float, nargs='+',
+                        default=[0.15, 0.25, 0.50, 0.75, 0.85],
+                        help='Drop probabilities to sweep over')
+    parser.add_argument('-v', '--verbose', action='store_true', default=True,
+                        help='Verbose')
+    args = parser.parse_args()
+    main(args)
