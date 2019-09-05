affd95e795a4509e23430c84b8a76c73b7669973
Deepak Narayanan <deepakn94@gmail.com>
2019-09-05 10:05:52 -0700

    Set co-located throughputs to 0 if jobs have different scale factors (since we don't pack them anyway)

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -977,15 +977,21 @@ class Scheduler:
                 # so make sure the co-located throughputs match the order of the
                 # single-job IDs.
                 if job_id [0] == merged_job_id[0]:
-                    self._throughputs[merged_job_id][worker_type] = \
-                        self._compute_throughput(
-                            [job.job_type, other_job.job_type],
-                            worker_type)
+                    if job.scale_factor != other_job.scale_factor:
+                        self._throughputs[merged_job_id][worker_type] = 0.0
+                    else:
+                        self._throughputs[merged_job_id][worker_type] = \
+                            self._compute_throughput(
+                                [job.job_type, other_job.job_type],
+                                worker_type)
                 else:
-                    self._throughputs[merged_job_id][worker_type] = \
-                        self._compute_throughput(
-                            [other_job.job_type, job.job_type],
-                            worker_type)
+                    if job.scale_factor != other_job.scale_factor:
+                        self._throughputs[merged_job_id][worker_type] = 0.0
+                    else:
+                        self._throughputs[merged_job_id][worker_type] = \
+                            self._compute_throughput(
+                                [other_job.job_type, job.job_type],
+                                worker_type)
 
                 self._steps_run_so_far[merged_job_id][worker_type] = 0
 
