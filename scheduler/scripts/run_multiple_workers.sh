2835ef0d70b620fabe836db636da487356dc6c2f
Ubuntu <ubuntu@ip-192-168-89-161.ec2.internal>
2019-09-19 22:15:53 +0000

    Add argument to run_multiple_workers.sh for min GPU id

--- scheduler/scripts/run_multiple_workers.sh
+++ scheduler/scripts/run_multiple_workers.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [ "$#" -ne 5 ]; then
-  echo "Usage: ./run_multiple_workers.sh [num_workers] [ip_addr] [worker_type] [logdir] [time_per_iteration]"
+if [ "$#" -ne 6 ]; then
+  echo "Usage: ./run_multiple_workers.sh [num_workers] [ip_addr] [worker_type] [logdir] [time_per_iteration] [min_gpu_id]"
   exit
 fi
 
 let port=60061
 let max_id=$1-1
-for i in $(seq 0 $max_id) 
+let min_id=$6
+for i in $(seq $min_id $max_id)
 do
   python worker.py -i $2 -t $3 -g $i -w $port --time_per_iteration $5 > $4/worker_$i.log 2>&1 &
   let port=$port+1
