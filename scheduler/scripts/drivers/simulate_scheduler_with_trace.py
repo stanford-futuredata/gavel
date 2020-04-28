6940313a6984230e7645eb57e458b4369a1b83f3
Deepak Narayanan <deepakn94@gmail.com>
2020-04-28 11:14:38 -0700

    Remove full file path for oracle throughputs file

--- scheduler/scripts/drivers/simulate_scheduler_with_trace.py
+++ scheduler/scripts/drivers/simulate_scheduler_with_trace.py
@@ -45,8 +45,7 @@ if __name__=='__main__':
                         choices=utils.get_available_policies(),
                         help='Scheduler policy')
     parser.add_argument('--throughputs_file', type=str,
-                        default=('/lfs/1/keshav2/gpusched/scheduler/'
-                                 'oracle_throughputs.json'),
+                        default='oracle_throughputs.json',
                         help='Oracle throughputs file')
     parser.add_argument('-c', '--cluster_spec', type=str, default='25:0:0',
                         help=('Cluster specification in the form of '
