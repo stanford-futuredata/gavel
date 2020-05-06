d405133c3766b195663e00739aa4376828dcf8e0
Deepak Narayanan <deepakn94@gmail.com>
2020-05-06 11:23:50 -0700

    New multi-entity fairness policy

--- scheduler/tests/policies_tests.py
+++ scheduler/tests/policies_tests.py
@@ -1,7 +1,7 @@
 import sys; sys.path.append("..")
 from job_id_pair import JobIdPair
 from policies import allox, finish_time_fairness, gandiva, isolated, \
-    max_min_fairness, max_sum_throughput
+    max_min_fairness, max_min_fairness_multi_entity, max_sum_throughput
 
 import itertools
 import numpy as np
@@ -161,6 +161,31 @@ class TestPolicies(unittest.TestCase):
         }
         policy.get_allocation(unflattened_throughputs, scale_factors,
                               unflattened_priority_weights,
+                              cluster_spec)
+
+    def test_max_min_fairness_multi_entity_with_perf(self):
+        policy = max_min_fairness_multi_entity.MaxMinFairnessMultiEntityPolicyWithPerf(
+            solver='ECOS')
+        unflattened_throughputs = {
+            0: {'v100': 2.0, 'p100': 1.0, 'k80': 0.5},
+            1: {'v100': 3.0, 'p100': 2.0, 'k80': 1.0},
+            2: {'v100': 3.0, 'p100': 2.0, 'k80': 1.0},
+        }
+        scale_factors = {
+            0: 1,
+            1: 1,
+            2: 1
+        }
+        unflattened_priority_weights = {'A': 1, 'B': 3}
+        job_to_entity_mapping = {0: 'A', 1: 'B', 2: 'B'}
+        cluster_spec = {
+            'v100': 1,
+            'p100': 2,
+            'k80': 3
+        }
+        policy.get_allocation(unflattened_throughputs, scale_factors,
+                              unflattened_priority_weights,
+                              job_to_entity_mapping,
                               cluster_spec)
 
     def test_max_min_fairness_with_packing(self):
