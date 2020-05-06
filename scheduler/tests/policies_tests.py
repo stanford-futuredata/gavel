ca254cea82c3998f8bd7a6002b91e4bd95274ded
Deepak Narayanan <deepakn94@gmail.com>
2020-05-06 12:32:51 -0700

    First pass at hierarchical policy

--- scheduler/tests/policies_tests.py
+++ scheduler/tests/policies_tests.py
@@ -1,6 +1,6 @@
 import sys; sys.path.append("..")
 from job_id_pair import JobIdPair
-from policies import allox, finish_time_fairness, gandiva, isolated, \
+from policies import allox, finish_time_fairness, gandiva, hierarchical, isolated, \
     max_min_fairness, max_min_fairness_multi_entity, max_sum_throughput
 
 import itertools
@@ -269,6 +269,31 @@ class TestPolicies(unittest.TestCase):
         policy.get_allocation_using_job_type_throughputs(
                 unflattened_job_type_throughputs, job_id_to_job_type,
                 scale_factors, unflattened_priority_weights, cluster_spec)
+
+    def test_two_level_single_pass_hierarchical(self):
+        policy = hierarchical.TwoLevelSinglePassHierarchicalPolicy(
+            solver='ECOS')
+        unflattened_throughputs = {
+            0: {'v100': 2.0, 'p100': 1.0, 'k80': 0.5},
+            1: {'v100': 3.0, 'p100': 2.0, 'k80': 1.0},
+            2: {'v100': 3.0, 'p100': 2.0, 'k80': 1.0},
+        }
+        scale_factors = {
+            0: 1,
+            1: 1,
+            2: 1
+        }
+        unflattened_priority_weights = {'A': 1, 'B': 3}
+        job_to_entity_mapping = {0: 'A', 1: 'B', 2: 'B'}
+        cluster_spec = {
+            'v100': 1,
+            'p100': 2,
+            'k80': 3
+        }
+        policy.get_allocation(unflattened_throughputs, scale_factors,
+                              unflattened_priority_weights,
+                              job_to_entity_mapping,
+                              cluster_spec)
 
     def test_finish_time_fairness(self):
         policy = finish_time_fairness.FinishTimeFairnessPolicy(
