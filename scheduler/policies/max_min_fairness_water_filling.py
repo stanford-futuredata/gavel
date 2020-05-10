d8302dda3fc4b8c60f3db82dd45c360cddb023ef
Deepak Narayanan <deepakn94@gmail.com>
2020-05-09 19:20:26 -0700

    Working version of water filling algorithm

--- scheduler/policies/max_min_fairness_water_filling.py
+++ scheduler/policies/max_min_fairness_water_filling.py
@@ -17,10 +17,24 @@ class MaxMinFairnessWaterFillingPolicyWithPerf(Policy):
         self._proportional_policy = ProportionalPolicy()
 
     def _get_allocation_helper(self, throughputs, priority_weights,
-                               scale_factors_array, m, n, x_so_far):
+                               scale_factors_array, m, n, x_so_far,
+                               index_to_check=None, computed_c=None):
         x = cp.Variable(throughputs.shape)
         c = cp.Variable()
-        objective = cp.Maximize(c)
+
+        # Multiply throughputs by scale_factors to ensure that scale_factor
+        # is taken into account while allocating times to different jobs.
+        # A job run on 1 GPU should receive `scale_factor` more time than
+        # a job run on `scale_factor` GPUs if throughputs are equal.
+        scaled_effective_throughputs = cp.sum(cp.multiply(
+            np.multiply(throughputs * priority_weights.reshape((m, 1)),
+                        scale_factors_array), x), axis=1)
+
+        if index_to_check is None:
+            objective = cp.Maximize(c)
+        else:
+            assert computed_c is not None
+            objective = cp.Maximize(scaled_effective_throughputs[index_to_check])
 
         # Multiply throughputs by scale_factors to ensure that scale_factor
         # is taken into account while allocating times to different jobs.
@@ -32,14 +46,17 @@ class MaxMinFairnessWaterFillingPolicyWithPerf(Policy):
 
         # Make sure that the allocation can fit in the cluster.
         constraints = self.get_base_constraints(x, scale_factors_array, x_so_far)
-        constraints += [scaled_effective_throughputs == c]
+        if index_to_check is not None:
+            constraints += [scaled_effective_throughputs >= computed_c]
+        else:
+            constraints += [scaled_effective_throughputs == c]
         cvxprob = cp.Problem(objective, constraints)
         result = cvxprob.solve(solver=self._solver)
 
         if cvxprob.status != "optimal":
             print('WARNING: Allocation returned by policy not optimal!')
 
-        return x
+        return x, objective.value
 
     def get_allocation(self, original_unflattened_throughputs, scale_factors,
                        unflattened_priority_weights, original_cluster_spec):
@@ -79,8 +96,10 @@ class MaxMinFairnessWaterFillingPolicyWithPerf(Policy):
                     original_i = original_job_ids.index(job_id)
                     original_j = original_worker_types.index(worker_type)
                     x_so_far[i][j] = x_final[original_i][original_j]
-            x = self._get_allocation_helper(
+            x, c = self._get_allocation_helper(
                 throughputs, priority_weights, scale_factors_array, m, n, x_so_far)
+            if num_iterations == 0:
+                print("Objective value: %.3f" % c)
             for i, job_id in enumerate(job_ids):
                 for j, worker_type in enumerate(worker_types):
                     original_i = original_job_ids.index(job_id)
@@ -88,33 +107,56 @@ class MaxMinFairnessWaterFillingPolicyWithPerf(Policy):
                     x_final[original_i][original_j] += x.value[i][j]
             num_resources_used = np.multiply(x.value, scale_factors_array).sum(axis=0)
 
+            # Adjust number of resources available for next iteration.
             worker_types_ordered = ['v100', 'p100', 'k80']
-            worker_type_indices = {
-                worker_types_ordered[i]: [worker_types.index(worker_type)
-                                          for worker_type in worker_types_ordered[:i+1]]
-                for i in range(len(worker_types_ordered))}
             job_id_set = set()
             for worker_type in worker_types_ordered:
                 i = worker_types.index(worker_type)
                 if cluster_spec[worker_type] > 0:
-                    if num_resources_used[i] / cluster_spec[worker_type] < 0.999:
-                        print("Resource %s underused (fraction used = %.2f)!" % (
-                            worker_type, num_resources_used[i] / cluster_spec[worker_type]))
                     cluster_spec[worker_type] -= num_resources_used[i]
                     if cluster_spec[worker_type] < 1e-1:
                         cluster_spec[worker_type] = 0
-            # TODO: Other job_ids might be bottleneck jobs as well.
+
+            # Find bottleneck job_ids.
             for i, job_id in enumerate(job_ids):
-                if x_final[original_job_ids.index(job_id)][original_worker_types.index("v100")] > 0.999:
+                # Find maximum scaled effective throughput for this job.
+                _, max_c_for_i = self._get_allocation_helper(
+                    throughputs, priority_weights, scale_factors_array, m, n,
+                    x_so_far, index_to_check=i, computed_c=c)
+                # If maximum scaled effective throughput for this job is near
+                # this iteration's max-min objective, this job is a bottleneck.
+                if 0.99 <= max_c_for_i / c <= 1.01:
                     job_id_set.add(job_id)
             if len(job_id_set) == 0:
                 done = True
             for job_id in job_id_set:
                 del unflattened_throughputs[job_id]
             num_iterations += 1
-            if len(unflattened_throughputs) == 0 or np.all([cluster_spec[worker_type] == 0 for worker_type in worker_types]):
+            if len(unflattened_throughputs) == 0 or \
+                np.all([cluster_spec[worker_type] == 0
+                        for worker_type in worker_types]):
                 done = True
-            print()
         print("Number of iterations: %d" % num_iterations)
+
+        (m, n) = len(original_job_ids), len(original_worker_types)
+        scale_factors_array = self.scale_factors_array(
+            scale_factors, original_job_ids, m, n)
+
+        priority_weights = np.array(
+            [1. / unflattened_priority_weights[job_id]
+             for job_id in original_job_ids])
+
+        proportional_throughputs = self._proportional_policy.get_throughputs(
+            original_throughputs, original_index, original_cluster_spec)
+        priority_weights = np.multiply(priority_weights.reshape((m, 1)),
+                                       1.0 / proportional_throughputs.reshape((m, 1)))
+        scaled_effective_throughputs = np.sum(np.multiply(
+            np.multiply(original_throughputs * priority_weights.reshape((m, 1)),
+                        scale_factors_array), x_final), axis=1)
+
+        print(scaled_effective_throughputs)
+        print("Final objective: %.3f" % np.min(scaled_effective_throughputs))
+        print(np.multiply(x_final, scale_factors_array).sum(axis=0),
+              x_final.sum(axis=1))
 
         return super().unflatten(x_final.clip(min=0.0).clip(max=1.0), index)
--- scheduler/tests/water_filling_tests.py
+++ scheduler/tests/water_filling_tests.py
@@ -3,27 +3,32 @@ from policies import max_min_fairness_water_filling
 
 import random
 
-def test_two_level_single_pass_hierarchical():
+def test_water_filling():
     policy = max_min_fairness_water_filling.MaxMinFairnessWaterFillingPolicyWithPerf(
         solver='ECOS')
     worker_types = ['v100', 'p100', 'k80']
     cluster_spec = {worker_type: 32 for worker_type in worker_types}
     num_jobs = random.randint(1, 100)
+    print("Total number of jobs: %d" % num_jobs)
     unflattened_throughputs = {}
     scale_factors = {}
     unflattened_priority_weights = {}
+    num_workers_requested = 0
     for i in range(num_jobs):
         throughputs = [random.random() for i in range(len(worker_types))]
         throughputs.sort(reverse=True)
         unflattened_throughputs[i] = {
             worker_types[i]: throughputs[i] for i in range(len(worker_types))}
         scale_factors[i] = 2 ** random.choice(range(4))
+        num_workers_requested += scale_factors[i]
         unflattened_priority_weights[i] = random.randint(1, 5)
+    print("Total number of workers requested: %d" % num_workers_requested)
     allocation = policy.get_allocation(unflattened_throughputs, scale_factors,
                                        unflattened_priority_weights,
                                        cluster_spec)
-    return allocation
+    print()
 
 
 if __name__ == '__main__':
-    allocation = test_two_level_single_pass_hierarchical()
+    for i in range(50):
+        test_water_filling()
