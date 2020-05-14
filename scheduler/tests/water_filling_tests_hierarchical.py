3b271c857a8269788205c3c0c256ac9a62660c97
Deepak Narayanan <deepakn94@gmail.com>
2020-05-14 14:12:05 -0700

    Clean test driver file

--- scheduler/tests/water_filling_tests_hierarchical.py
+++ scheduler/tests/water_filling_tests_hierarchical.py
@@ -11,9 +11,9 @@ def test_water_filling_multilevel(priority_reweighting_policy):
     policy = max_min_fairness_water_filling.MaxMinFairnessWaterFillingPolicyWithPerf(
         priority_reweighting_policy=priority_reweighting_policy)
     worker_types = ['k80', 'p100', 'v100']
-    cluster_spec = {worker_type: 2 for worker_type in worker_types}
-    num_jobs = 10
-    num_entities = 2
+    cluster_spec = {worker_type: 64 for worker_type in worker_types}
+    num_jobs = 300
+    num_entities = 10
     print("Total number of jobs: %d" % num_jobs)
     print("Total number of entities: %d" % num_entities)
     unflattened_throughputs = {}
@@ -24,25 +24,21 @@ def test_water_filling_multilevel(priority_reweighting_policy):
     for i in range(num_entities):
         entity_id = 'entity%d' % i
         entity_to_job_mapping[entity_id] = []
-        # unflattened_priority_weights[entity_id] = random.randint(1, 5)
-        unflattened_priority_weights[entity_id] = random.randint(1, 2)
+        unflattened_priority_weights[entity_id] = random.randint(1, 5)
         print("Entity %d: Priority: %d" % (
             i, unflattened_priority_weights[entity_id]))
     for i in range(num_jobs):
-        # throughputs = [random.random() for i in range(len(worker_types))]
-        throughputs = [3.0, 2.0, 1.0]
+        throughputs = [random.random() for i in range(len(worker_types))]
         throughputs.sort()
         unflattened_throughputs[i] = {
             worker_types[i]: throughputs[i] for i in range(len(worker_types))}
-        # scale_factors[i] = 2 ** random.randint(0, 2)
-        scale_factors[i] = 1
+        scale_factors[i] = 2 ** random.randint(0, 2)
         num_workers_requested += scale_factors[i]
-        priority_weight = random.randint(1, 2)
+        priority_weight = random.randint(1, 5)
         if priority_reweighting_policy == 'fairness+fifo':
             priority_weight = 1.0
         unflattened_priority_weights[i] = priority_weight
-        # entity_id = 'entity%d' % random.randint(0, num_entities-1)
-        entity_id = 'entity%d' % (i // 5)
+        entity_id = 'entity%d' % random.randint(0, num_entities-1)
         entity_to_job_mapping[entity_id].append(i)
         print("Job %d: Throughputs: %s, Priority: %d, Scale factor: %d, Entity: %s" % (
             i, unflattened_throughputs[i], unflattened_priority_weights[i],
@@ -51,8 +47,7 @@ def test_water_filling_multilevel(priority_reweighting_policy):
     allocation = policy.get_allocation(unflattened_throughputs, scale_factors,
                                        unflattened_priority_weights,
                                        cluster_spec,
-                                       entity_to_job_mapping=entity_to_job_mapping,
-                                       verbose=True)
+                                       entity_to_job_mapping=entity_to_job_mapping)
     print()
 
 
@@ -60,7 +55,7 @@ if __name__ == '__main__':
     seed = 0
     random.seed(seed)
     start_time = time.time()
-    priority_reweighting_policy = 'fairness+fifo'
+    priority_reweighting_policy = 'multilevel_fairness'
     for i in range(5):
         test_water_filling_multilevel(priority_reweighting_policy)
     print("Average time per problem: %.2f seconds" % ((time.time() - start_time) / 5))
