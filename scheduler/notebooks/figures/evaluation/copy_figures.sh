c11b15f8421cb5087c9e5bbbe551a23e0b9867fe
Deepak Narayanan <deepakn94@gmail.com>
2020-05-20 13:25:05 -0700

    Some fixes

--- scheduler/notebooks/figures/evaluation/copy_figures.sh
+++ scheduler/notebooks/figures/evaluation/copy_figures.sh
@@ -3,5 +3,4 @@ cp -r fairness_with_priorities ~/Documents/research/gpusched_papers/osdi20/figur
 cp -r makespan ~/Documents/research/gpusched_papers/osdi20/figures
 cp -r philly ~/Documents/research/gpusched_papers/osdi20/figures
 cp -r runtimes ~/Documents/research/gpusched_papers/osdi20/figures
-cp -r throughput_estimation ~/Documents/research/gpusched_papers/osdi20/figures
 cp -r time_per_iteration ~/Documents/research/gpusched_papers/osdi20/figures
