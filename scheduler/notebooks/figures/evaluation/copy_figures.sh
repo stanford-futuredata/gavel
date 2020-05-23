b4af3ac8d3f64d2e2bc68917910eb5f48f7ae7ca
Deepak Narayanan <deepakn94@gmail.com>
2020-05-22 21:31:24 -0700

    Change in copy_figures.sh script

--- scheduler/notebooks/figures/evaluation/copy_figures.sh
+++ scheduler/notebooks/figures/evaluation/copy_figures.sh
@@ -1,5 +1,4 @@
 cp -r continuous_jobs ~/Documents/research/gpusched_papers/osdi20/figures
-cp -r fairness_with_priorities ~/Documents/research/gpusched_papers/osdi20/figures
 cp -r makespan ~/Documents/research/gpusched_papers/osdi20/figures
 cp -r philly ~/Documents/research/gpusched_papers/osdi20/figures
 cp -r solver_scaling ~/Documents/research/gpusched_papers/osdi20/figures
