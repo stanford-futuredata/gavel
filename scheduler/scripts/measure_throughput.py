2c98cdc143e90ead9da0cf22fe157bd1a5b830f6
Fiodar Kazhamiaka <fiodar@dawn7.stanford.edu>
2020-05-26 09:48:47 -0700

    lower batch size

--- scheduler/scripts/measure_throughput.py
+++ scheduler/scripts/measure_throughput.py
@@ -205,7 +205,7 @@ job_table = [
 
 
 job_table = [
-    Job(model='ResNet-50 (batch size 128)',
+    Job(model='ResNet-50 (batch size 64)',
         command=('cd %s/gpusched/workloads/pytorch/'
                  'image_classification/imagenet && python '
                  'main.py -j 4 -a resnet50 --synth -b 59 %s/data/imagenet/pytorch '
