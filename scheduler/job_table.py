d605a82edb7544c96e851650890a16bf8985c125
Keshav Santhanam <keshav2@stanford.edu>
2020-08-29 17:24:16 -0700

    Update job table

--- scheduler/job_table.py
+++ scheduler/job_table.py
@@ -1,144 +1,95 @@
+import os
+import sys
+
 from job_template import JobTemplate
 
-JobTable = [
-    JobTemplate(model='ResNet-18 (batch size 16)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'image_classification/cifar10 && python3 '
-                       'main.py --data_dir=%s/data/cifar10 --batch_size 16 '),
-              num_steps_arg='--num_steps', distributed=True),
-    JobTemplate(model='ResNet-18 (batch size 32)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'image_classification/cifar10 && python3 '
-                       'main.py --data_dir=%s/data/cifar10 --batch_size 32 '),
-              num_steps_arg='--num_steps', distributed=True),
-    JobTemplate(model='ResNet-18 (batch size 64)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'image_classification/cifar10 && python3 '
-                       'main.py --data_dir=%s/data/cifar10 --batch_size 64 '),
-              num_steps_arg='--num_steps', distributed=True),
-    JobTemplate(model='ResNet-18 (batch size 128)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'image_classification/cifar10 && python3 '
-                       'main.py --data_dir=%s/data/cifar10 --batch_size 128 '),
-              num_steps_arg='--num_steps', distributed=True),
-    JobTemplate(model='ResNet-18 (batch size 256)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'image_classification/cifar10 && python3 '
-                       'main.py --data_dir=%s/data/cifar10 --batch_size 256 '),
-              num_steps_arg='--num_steps', distributed=True),
-    JobTemplate(model='ResNet-50 (batch size 16)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'image_classification/imagenet && python3 '
-                       'main.py -j 8 -a resnet50 -b 16 '
-                       '%s/data/imagenet/pytorch'),
-              num_steps_arg='--num_minibatches', distributed=True),
-    JobTemplate(model='ResNet-50 (batch size 32)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'image_classification/imagenet && python3 '
-                       'main.py -j 8 -a resnet50 -b 32 '
-                       '%s/data/imagenet/pytorch'),
-              num_steps_arg='--num_minibatches', distributed=True),
-    JobTemplate(model='ResNet-50 (batch size 64)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'image_classification/imagenet && python3 '
-                       'main.py -j 8 -a resnet50 -b 64 '
-                       '%s/data/imagenet/pytorch'),
-              num_steps_arg='--num_minibatches', distributed=True),
-    JobTemplate(model='ResNet-50 (batch size 128)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'image_classification/imagenet && python3 '
-                       'main.py -j 8 -a resnet50 -b 128 '
-                       '%s/data/imagenet/pytorch'),
-              num_steps_arg='--num_minibatches', distributed=True),
-    JobTemplate(model='Transformer (batch size 16)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'translation && python3 train.py -data '
-                       '%s/data/translation/multi30k.atok.low.pt '
-                       '-batch_size 16 -proj_share_weight'),
-              num_steps_arg='-step', distributed=True),
-    JobTemplate(model='Transformer (batch size 32)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'translation && python3 train.py -data '
-                       '%s/data/translation/multi30k.atok.low.pt '
-                       '-batch_size 32 -proj_share_weight'),
-              num_steps_arg='-step', distributed=True),
-    JobTemplate(model='Transformer (batch size 64)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'translation && python3 train.py -data '
-                       '%s/data/translation/multi30k.atok.low.pt '
-                       '-batch_size 64 -proj_share_weight'),
-              num_steps_arg='-step', distributed=True),
-    JobTemplate(model='Transformer (batch size 128)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'translation && python3 train.py -data '
-                       '%s/data/translation/multi30k.atok.low.pt '
-                       '-batch_size 128 -proj_share_weight'),
-              num_steps_arg='-step', distributed=True),
-    JobTemplate(model='Transformer (batch size 256)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'translation && python3 train.py -data '
-                       '%s/data/translation/multi30k.atok.low.pt '
-                       '-batch_size 256 -proj_share_weight'),
-              num_steps_arg='-step', distributed=True),
-    JobTemplate(model='A3C',
-              command=('cd %s/gpusched/workloads/pytorch/rl && '
-                       'python3 main.py --env PongDeterministic-v4 --workers 4 '
-                       '--amsgrad True'),
-              num_steps_arg='--max-steps',
-              needs_data_dir=False),
-    JobTemplate(model='LM (batch size 5)',
-                command=('cd %s/gpusched/workloads/pytorch/'
-                         'language_modeling && python main.py --cuda --data '
-                         '%s/data/wikitext2 --batch_size 5'),
-                num_steps_arg='--steps', distributed=True),
-    JobTemplate(model='LM (batch size 10)',
-                command=('cd %s/gpusched/workloads/pytorch/'
-                         'language_modeling && python main.py --cuda --data '
-                         '%s/data/wikitext2 --batch_size 10'),
-                num_steps_arg='--steps', distributed=True),
-    JobTemplate(model='LM (batch size 20)',
-                command=('cd %s/gpusched/workloads/pytorch/'
-                         'language_modeling && python main.py --cuda --data '
-                         '%s/data/wikitext2 --batch_size 20'),
-                num_steps_arg='--steps', distributed=True),
-    JobTemplate(model='LM (batch size 40)',
-                command=('cd %s/gpusched/workloads/pytorch/'
-                         'language_modeling && python main.py --cuda --data '
-                         '%s/data/wikitext2 --batch_size 40'),
-                num_steps_arg='--steps', distributed=True),
-    JobTemplate(model='LM (batch size 80)',
-                command=('cd %s/gpusched/workloads/pytorch/'
-                         'language_modeling && python main.py --cuda --data '
-                         '%s/data/wikitext2 --batch_size 80'),
-                num_steps_arg='--steps', distributed=True),
-    JobTemplate(model='Recommendation (batch size 512)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'recommendation/ && python3 train.py '
-                       '--data_dir %s/data/ml-20m/pro_sg/ --batch_size 512'),
-              num_steps_arg='-n'),
-    JobTemplate(model='Recommendation (batch size 1024)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'recommendation/ && python3 train.py '
-                       '--data_dir %s/data/ml-20m/pro_sg/ --batch_size 1024'),
-              num_steps_arg='-n'),
-    JobTemplate(model='Recommendation (batch size 2048)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'recommendation/ && python3 train.py '
-                       '--data_dir %s/data/ml-20m/pro_sg/ --batch_size 2048'),
-              num_steps_arg='-n'),
-    JobTemplate(model='Recommendation (batch size 4096)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'recommendation/ && python3 train.py '
-                       '--data_dir %s/data/ml-20m/pro_sg/ --batch_size 4096'),
-              num_steps_arg='-n'),
-    JobTemplate(model='Recommendation (batch size 8192)',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'recommendation/ && python3 train.py '
-                       '--data_dir %s/data/ml-20m/pro_sg/ --batch_size 8192'),
-              num_steps_arg='-n'),
-    JobTemplate(model='CycleGAN',
-              command=('cd %s/gpusched/workloads/pytorch/'
-                       'cyclegan && python3 cyclegan.py --dataset_path '
-                       '%s/data/monet2photo --decay_epoch 0'),
-              num_steps_arg='--n_steps'),
-]
+def resnet18(batch_size, gavel_dir):
+    model = 'ResNet18 (batch size %d)' % (batch_size)
+    command = 'python3 main.py --data_dir=%s/data/cifar10'
+    command += ' --batch_size %d' % (batch_size)
+    working_directory = \
+        '%s/workloads/pytorch/image_classification/cifar10' % (gavel_dir)
+    num_steps_arg = '--num_steps'
+    return JobTemplate(model=model, command=command,
+                       working_directory=working_directory,
+                       num_steps_arg=num_steps_arg, distributed=True)
+
+def resnet50(batch_size, gavel_dir):
+    model = 'ResNet50 (batch size %d)' % (batch_size)
+    command = 'python3 main.py -j 8 -a resnet50 -b %d' % (batch_size)
+    command += ' %s/data/imagenet/pytorch'
+    working_directory = \
+        '%s/workloads/pytorch/image_classification/imagenet' % (gavel_dir)
+    num_steps_arg = '--num_minibatches'
+    return JobTemplate(model=model, command=command,
+                       working_directory=working_directory,
+                       num_steps_arg=num_steps_arg, distributed=True)
+
+def transformer(batch_size, gavel_dir):
+    model = 'Transformer (batch size %d)' % (batch_size)
+    command = 'python3 train.py -data %s/data/translation/multi30k.atok.low.pt'
+    command += '-batch_size %d -proj_share_weight' % (batch_size)
+    working_directory = '%s/workloads/pytorch/translation' % (gavel_dir)
+    num_steps_arg = '-step'
+    return JobTemplate(model=model, command=command,
+                       working_directory=working_directory,
+                       num_steps_arg=num_steps_arg, distributed=True)
+
+def lm(batch_size, gavel_dir):
+    model = 'LM (batch size %d)' % (batch_size)
+    command = 'python main.py --cuda --data %s/data/wikitext2'
+    command += ' --batch_size %d' % (batch_size)
+    working_directory = '%s/workloads/pytorch/language_modeling' % (gavel_dir)
+    num_steps_arg = '--steps'
+    return JobTemplate(model=model, command=command,
+                       working_directory=working_directory,
+                       num_steps_arg=num_steps_arg, distributed=True)
+
+def recommendation(batch_size, gavel_dir):
+    model = 'Recommendation (batch size %d)' % (batch_size)
+    command = 'python3 train.py --data_dir %s/data/ml-20m/pro_sg/'
+    command += ' --batch_size %d' % (batch_size)
+    working_directory = '%s/workloads/pytorch/recommendation' % (gavel_dir)
+    num_steps_arg = '-n'
+    return JobTemplate(model=model, command=command,
+                       working_directory=working_directory,
+                       num_steps_arg=num_steps_arg)
+
+def a3c(gavel_dir):
+    model = 'A3C'
+    command = ('python3 main.py --env PongDeterministic-v4 --workers 4 '
+               '--amsgrad True')
+    working_directory = '%s/workloads/pytorch/rl' % (gavel_dir)
+    num_steps_arg = '--max_steps'
+    return JobTemplate(model=model, command=command,
+                       working_directory=working_directory,
+                       num_steps_arg=num_steps_arg,
+                       needs_data_dir=False)
+
+def cyclegan(gavel_dir):
+    model = 'CycleGAN'
+    working_directory = '%s/workloads/pytorch/cyclegan' % (gavel_dir)
+    command = ('python3 cyclegan.py --dataset_path %s/data/monet2photo'
+               ' --decay_epoch 0')
+    num_steps_arg = '--n_steps'
+    return JobTemplate(model=model, command=command,
+                       working_directory=working_directory,
+                       num_steps_arg=num_steps_arg)
+
+JobTable = []
+
+file_path = os.path.realpath(__file__)
+gavel_dir = os.path.basename(os.path.dirname(os.path.dirname(file_path)))
+
+for batch_size in [16, 32, 64, 128, 256]:
+    JobTable.append(resnet18(batch_size, gavel_dir))
+for batch_size in [16, 32, 64, 128]:
+    JobTable.append(resnet50(batch_size, gavel_dir))
+for batch_size in [16, 32, 64, 128, 256]:
+    JobTable.append(transformer(batch_size, gavel_dir))
+for batch_size in [5, 10, 20, 40, 80]:
+    JobTable.append(lm(batch_size, gavel_dir))
+for batch_size in [512, 1024, 2048, 4096, 8192]:
+    JobTable.append(recommendation(batch_size, gavel_dir))
+JobTable.append(a3c(gavel_dir))
+JobTable.append(cyclegan(gavel_dir))
